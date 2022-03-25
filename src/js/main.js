31f2149e1e6ee921b46b9ebd7006a346a9c53de4
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-03-26 00:36:20 +0530

    refactor: generating buttons with lifts

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 const floorPlan = [];
 
+// if i = floorPlan.length - 1
+const upButtonList = `<ul class="button-list"><button class="hidden">u</button><button>d</button></ul>`;
+// else if i = 0
+const downButtonList = `<ul class="button-list"><button>u</button><button class="hidden">d</button></ul>`;
+// else
+const normalButtonList = `<ul class="button-list"><button>u</button><button class>d</button></ul>`;
+
+// if currentFloor[j] === 1
+const visibleLift = `<div class="lift"></div>`;
+// else
+const hiddenLift = `<div class="hidden lift"></div>`;
+
 function setFloorPlan() {
   const floorCount = parseInt(document.getElementById("floors").value);
   const liftCount = parseInt(document.getElementById("lifts").value);
@@ -7,29 +19,49 @@ function setFloorPlan() {
   for (let i = 1; i < floorCount; i++) {
     floorPlan.push(Array(liftCount).fill(0));
   }
-
+  // to see currentFloorPlan
   window.sessionStorage.setItem("floorPlan", JSON.stringify(floorPlan));
 }
 
-function generateLiftsTemplate(currentFloor) {
-  let lift = `<ul class="lift-array">`;
+// continue from here
+// every package is a flex-h
+// composed of buttonList
+// and lift
+function generateLiftPackageArrTemplate(currentFloor, buttonList) {
+  let liftPackageArr = `<ul class="lift-array">`;
   for (let j = 0; j < currentFloor.length; ++j) {
     // generating lift plan
-    lift +=
-      currentFloor[j] === 1
-        ? `<div class="lift"></div>`
-        : `<div class="hidden-lift"></div>`;
+    let liftPackage = `<ul class="lift-package">`;
+    liftPackage += buttonList;
+    liftPackage += currentFloor[j] === 1 ? visibleLift : hiddenLift;
+    liftPackage += `</ul>`;
+    liftPackageArr += liftPackage;
   }
-  lift += `</ul>`;
-  return lift;
+  liftPackageArr += `</ul>`;
+  return liftPackageArr;
 }
 
 function generateUI() {
   let floors = "";
   for (let i = floorPlan.length - 1; i > -1; --i) {
     const floorNumTemplate = `<small class="floorNumber">${i}</small>`;
-    const liftTemplate = generateLiftsTemplate(floorPlan[i]);
-    floors += `<div class="line">${floorNumTemplate}${liftTemplate}</div>`;
+    let buttonList = "";
+    switch (i) {
+      case floorPlan.length - 1:
+        buttonList = upButtonList;
+        break;
+      case 0:
+        buttonList = downButtonList;
+        break;
+      default:
+        buttonList = normalButtonList;
+        break;
+    }
+    const liftPackageArrTemplate = generateLiftPackageArrTemplate(
+      floorPlan[i],
+      buttonList
+    );
+    floors += `<div class="line">${floorNumTemplate}${liftPackageArrTemplate}</div>`;
   }
   document.getElementById("floorplan").innerHTML = floors;
 }
