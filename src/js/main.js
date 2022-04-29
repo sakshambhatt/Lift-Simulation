7b3cb50e3679f58d6cc7bde83944e9a738ad4848
Saksham Bhatt <sakshambhatt33@gmail.com>
2022-04-30 05:13:01 +0530

    refactor: changed alt for down btns

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ function getButtonList(floorCount, floorNumber, liftNum) {
   const downButtonList = `<ul class="button-list"><button class="hidden">u</button><button onclick="callToFloor(
     ${floorNumber},
     ${liftNum}
-  )"><img src="../../assets/down.png" alt="up"/></button></ul>`;
+  )"><img src="../../assets/down.png" alt="down"/></button></ul>`;
   // else
   const normalButtonList = `<ul class="button-list"><button onclick="callToFloor(
     ${floorNumber},
@@ -19,7 +19,7 @@ function getButtonList(floorCount, floorNumber, liftNum) {
   )"><img src="../../assets/up.png" alt="up"/></button><button onclick="callToFloor(
     ${floorNumber},
     ${liftNum}
-  )"><img src="../../assets/down.png" alt="up"/></button></ul>`;
+  )"><img src="../../assets/down.png" alt="down"/></button></ul>`;
 
   let buttonList = "";
   switch (floorNumber) {
